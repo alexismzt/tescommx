a51f98618bf47a7a5712d983a652fd7017f4a9a4
alexis hernandez <hernandezalexmzt@gmail.com>
2018-08-20 23:59:48 -0600

    pagina

--- src/components/valores/objetivos.vue
+++ src/components/valores/objetivos.vue
@@ -3,7 +3,7 @@
     <div class="">
       <img src="https://picsum.photos/1024/480/?image=42" alt="">
     </div>
-    <h1 class="text-right">Objetivos</h1>
+    <h1 class="text-left">Objetivos</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </template>
@@ -24,7 +24,7 @@ export default {
 p{
   width: 40%;
   margin: 0 auto;
-  margin-right: 0px;
-  text-align: right;
+  margin-left: 0px;
+  text-align: left;
 }
 </style>
